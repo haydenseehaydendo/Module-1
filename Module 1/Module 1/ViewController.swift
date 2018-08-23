e7354e67177d4a250ef5a436f0e86cf0e1369b15
Dipesh Trikam <dipesht@datacom.co.nz>
2018-08-23 15:50:26 +1200

    Initial Project Files

---
+++ Module 1/Module 1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Module 1
+//
+//  Created by Dipesh Trikam on 23/08/18.
+//  Copyright © 2018 Dipesh Trikam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
